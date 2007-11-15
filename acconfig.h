ab62a79018a08a6955f7f1ac6d33f545997fd415
rodrigo.gallardo <rodrigo.gallardo@4c072952-ec3e-0410-8942-d3a44bbd62b3>
2007-11-15 00:44:04 +0000

    Imported state of original maintainer's 0.9.2

---
+++ acconfig.h
@@ -0,0 +1,29 @@
+/* $Id */
+/* general */
+#define PACKAGE "grun"
+#define VERSION "x.x.x"
+
+/* Define to 1 if NLS is requested.  */
+#undef ENABLE_NLS
+
+/* Define as 1 if you have catgets and don't want to use GNU gettext.  */
+#undef HAVE_CATGETS
+
+/* Define as 1 if you have gettext and don't want to use GNU gettext.  */
+#undef HAVE_GETTEXT
+
+/* Define if your locale.h file contains LC_MESSAGES.  */
+#undef HAVE_LC_MESSAGES
+
+/* Define if have stpcpy */
+#undef HAVE_STPCPY
+
+/* for testfile features */
+#undef TESTFILE
+#undef XTERM
+
+/* for file associations */
+#undef ASSOC
+
+/* for default prefix */
+#define PREFIX "/usr/local"
